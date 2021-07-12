857b80e844290c6a9e4b1ef20ba1dba2a0be6f73
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2021-07-12 00:43:04 -0300

    Adding socket io lib and starting SocketHelper.swift

---
+++ Socket-App/Socket-App/Models/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  Socket-App
+//
+//  Created by Marcus Vinicius Vieira Badiale on 12/07/21.
+//
+
+import Foundation
---
+++ Socket-App/Socket-App/Networking/SocketHelper.swift
@@ -0,0 +1,8 @@
+//
+//  SocketHelper.swift
+//  Socket-App
+//
+//  Created by Marcus Vinicius Vieira Badiale on 12/07/21.
+//
+
+import Foundation
