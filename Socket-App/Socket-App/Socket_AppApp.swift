eb5c8fb69916100ad3c66b4e260b43c236e99615
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2021-07-12 00:00:42 -0300

    Project setup.

---
+++ Socket-App/Socket-App/Socket_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Socket_AppApp.swift
+//  Socket-App
+//
+//  Created by Marcus Vinicius Vieira Badiale on 11/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Socket_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
