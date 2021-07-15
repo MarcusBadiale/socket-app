949af3d0ebae97ebf5de91e046119cb19f77d0a8
Marcus Vinicius Vieira Badiale <marcus.badiale@gmail.com>
2021-07-15 20:35:11 -0300

    Adding get all messages.

--- src-server-socket/index.js
+++ src-server-socket/index.js
@@ -3,10 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var userList = [];
-
-app.get('/', function(req, res){
-  res.send('<h1>AppCoda - SocketChat Server</h1>');
-});
+var allMessages = [];
 
 http.listen(3001, function(){
   console.log('Listening on *:3001');
@@ -44,10 +41,21 @@ io.on('connection', function(clientSocket){
 
 
   clientSocket.on('chatMessage', function(clientNickname, message){
+    var messageInfo = {};
     var currentDateTime = new Date().toLocaleString();
+
+    messageInfo["nickname"] = clientNickname
+    messageInfo["message"] = message
+    messageInfo["date"] = currentDateTime
+
+    allMessages.push(messageInfo);
+
     io.emit('newChatMessage', clientNickname, message, currentDateTime);
   });
 
+  clientSocket.on('messageList', function(clientNickname) {
+    io.emit("allMessages", allMessages);
+  });
 
   clientSocket.on("connectUser", function(clientNickname) {
       var message = "User " + clientNickname + " was connected.";
@@ -56,7 +64,7 @@ io.on('connection', function(clientSocket){
       var userInfo = {};
       var foundUser = false;
       for (var i=0; i<userList.length; i++) {
-        if (userList[i]["nickname"] == clientNickname) {
+        if (userList[i]["nickname"] == clientNickname) {;
           userList[i]["isConnected"] = true
           userList[i]["id"] = clientSocket.id;
           userInfo = userList[i];
